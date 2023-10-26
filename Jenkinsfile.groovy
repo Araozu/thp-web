bf031f3b1ee4c9e25be9166b35efda59913602d3
Araozu <faraoz@unsa.edu.pe>
2023-10-25 21:01:14 -0500

    jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,9 +11,16 @@ pipeline {
                 sh 'bun --version'
             }
         }
-        stage('Build') {
+        stage('Install') {
             steps {
                 sh 'bun install -p'
+            }
+        }
+        stage('Build') {
+            environment {
+                PATH = "${env.WORKSPACE}/node_modules/.bin:${env.PATH}"
+            }
+            steps {
                 sh 'bun tailwind:build'
                 sh 'md-docs'
             }
