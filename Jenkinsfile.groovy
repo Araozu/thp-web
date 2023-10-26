7871312233cbbde39cbbfb15b0bf178b1bf3f955
Araozu <faraoz@unsa.edu.pe>
2023-10-25 20:56:46 -0500

    jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,20 @@
 pipeline {
     agent any
     environment {
-        PATH = "/var/lib/jenkins/.bun/bin:${env.PATH}"
+        PATH = "/var/lib/jenkins/.bun/bin:/var/lib/jenkins/bin:${env.PATH}"
     }
     stages {
-        stage('Test') {
+        stage('Test env') {
             steps {
                 echo 'Testing...'
                 echo 'Is Bun working?...'
                 sh 'bun --version'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'bun tailwind:build'
+                sh 'md-docs'
             }
         }
     }
