18b79d60d054312ce1db4a39afcf6b0026c42671
Araozu <fernando@araozu.dev>
2024-03-27 21:02:52 -0500

    jenkins. again

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     environment {
-        PATH = "/var/lib/jenkins/.bun/bin/:/var/lib/jenkins/bin:${env.PATH}"
+        PATH = "/var/lib/jenkins/.bun/bin/:/var/lib/jenkins/bin:/var/lib/jenkins/.nvm/versions/node/v20.9.0/bin:${env.PATH}"
     }
     stages {
         stage('Install deps') {
