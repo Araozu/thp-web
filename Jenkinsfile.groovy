42b724966db43040a8677b923ce4ec29820d71f1
Araozu <fernando@araozu.dev>
2024-04-23 10:18:38 -0500

    Update jenkinsfile to build astro site

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,23 +9,14 @@ pipeline {
                 sh 'bun i'
             }
         }
-        stage('Build tailwind') {
-            environment {
-                PATH = "${env.WORKSPACE}/node_modules/.bin:${env.PATH}"
-            }
-            steps {
-                sh 'md-docs'
-                sh './node_modules/.bin/tailwindcss -i ./tailwind.css -o ./static/css/out.css --minify'
-            }
-        }
         stage('Build bundle') {
             steps {
-                sh 'bun bundle'
+                sh 'bun run build'
             }
         }
         stage('Deploy') {
             steps {
-                sh 'cp -r ./static/* /var/www/thp-docs/'
+                sh 'cp -r ./dist/* /var/www/thp-docs/'
             }
         }
     }
