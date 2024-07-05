49faed4fcb98bf807cab3d171925cac54daf75e9
Araozu <fernando@araozu.dev>
2024-07-05 16:58:50 -0500

    Move code snippets to use new component

--- src/layouts/thpHighlighter.ts
+++ src/layouts/thpHighlighter.ts
@@ -1,4 +1,15 @@
-import { thp_highlighter, CodeJar } from "../lexer/highlighter";
 
-export function highlightOnDom() {}
+export function highlightOnDom() {
+    const pre_elements = document.querySelectorAll("pre");
+    for (const pre_el of pre_elements) {
+        const language = pre_el.getAttribute("data-language");
+        if (language === null) { continue; }
+
+        // Create a visual indicador
+        const indicator = document.createElement("span");
+        indicator.className = "absolute top-2 right-2 inline-block text-sm select-none opacity-75";
+        indicator.innerText = language;
+        pre_el.appendChild(indicator);
+    }
+}
 
