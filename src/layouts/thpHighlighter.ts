aa357101fb179a0c42de95bfc7dc874fc2cf0135
Araozu <fernando@araozu.dev>
2024-07-19 22:08:37 -0500

    Add docs on templating

--- src/layouts/thpHighlighter.ts
+++ src/layouts/thpHighlighter.ts
@@ -7,9 +7,13 @@ export function highlightOnDom() {
 
         // Create a visual indicador
         const indicator = document.createElement("span");
-        indicator.className = `absolute top-1 right-0 inline-block text-sm select-none opacity-75 ${language === "php" ? "bg-[#4f5b93]" : ""} px-2 rounded-full`;
+
+        let indicator_bg_class = "";
+        if (language === "php") { indicator_bg_class = "bg-[#4f5b93]"; }
+        else if (language === "html") { indicator_bg_class = "bg-[#dc4a20]"; }
+
+        indicator.className = `absolute top-1 right-0 inline-block text-sm select-none opacity-85 ${indicator_bg_class} px-2 rounded-full`;
         indicator.innerText = language;
         pre_el.appendChild(indicator);
     }
 }
-
