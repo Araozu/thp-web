b43611ad90b6991301c9bc2d325c306f8b83c8cb
fernando <fernando@araozu.dev>
2024-10-04 20:54:44 +0000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,29 +1,11 @@
 pipeline {
-    agent any
-    environment {
-        PATH = "/var/lib/jenkins/bin:/var/lib/jenkins/.nvm/versions/node/v20.9.0/bin:${env.PATH}"
+    agent {
+        docker { image 'node:14-alpine' }
     }
     stages {
-        stage('Install deps') {
+        stage('Test') {
             steps {
-                sh 'pnpm i'
-            }
-        }
-        stage("Test") {
-            steps {
-                sh 'pnpm test'
-            }
-        }
-        stage('Build bundle') {
-            steps {
-                sh 'rm .env || true'
-                sh 'echo "THP_BINARY=/var/lib/jenkins/bin/thp" > .env'
-                sh 'pnpm build'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                sh 'cp -r ./dist/* /var/www/thp-docs/'
+                sh 'node --version'
             }
         }
     }
